6ad6b96cc6c6587c62c817537a38c9b963c499b7
Phil P <pepownall@gmail.com>
2026-01-16 15:47:24 -0500

    Update moon_icon.h
    Fixed New Moon Icon

--- common/moon_icon.h
+++ common/moon_icon.h
@@ -1,7 +1,7 @@
 // Thanks to https://community.home-assistant.io/u/Tofandel
 const char * moon_icon(int moon_age) {
 	if (moon_age < 2)
-       return "󰽧";  // moon-new
+       return "󰽤";  // moon-new
 	else if (moon_age < 7)
        return "󰽧"; // moon-waxing-crescent
 	else if (moon_age < 8)
@@ -17,5 +17,5 @@ const char * moon_icon(int moon_age) {
 	else if (moon_age < 28)
        return "󰽥"; // moon-waning-crescent
 	else
-       return "󰽧"; // moon-new
+       return "󰽤"; // moon-new
 }
