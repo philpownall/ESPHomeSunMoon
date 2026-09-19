0cfe5a5afd01f05fb09ade7c6b3669c7dd36f381
Phil P <pepownall@gmail.com>
2026-09-19 12:27:25 -0400

    Update astro.h
    One more time

--- common/astro.h
+++ common/astro.h
@@ -141,7 +141,9 @@ double moonrotation (double pa, double bla) {
     // This is the rotation angle of the moon with North pointing up
     // For the user's latitude
     // This is the view of the moon the user would see in the sky at his location
-    double rotation = -(bla - pa) - std::numbers::pi/2;
+    double rotation = 0;
+    if (bla <0) { rotation = (-(bla - pa) - std::numbers::pi/2);
+    } else { rotation = (-(bla - ) + std::numbers::pi/2); }
     return rotation;
 }
 std::string moonphasetext (int age) {
